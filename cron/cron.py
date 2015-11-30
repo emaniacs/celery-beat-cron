af7aec0958bfc0f39a9def6443942309c1880d6b
N.Ardi <noone.nu1@gmail.com>
2015-11-30 16:05:35 +0700

    add timeout and logging

--- cron/cron.py
+++ cron/cron.py
@@ -10,6 +10,9 @@ app.config_from_object('celeryconfig')
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
+# default timeout 10 seconds
+DEFAULT_TIMEOUT = 10
+
 @app.task
 def run_crontab(jobname):
     """
@@ -19,11 +22,34 @@ def run_crontab(jobname):
 
     name = str(jobname)
     filename = '{}/jobs/{}.py'.format(THIS_DIR, name)
+    log_it(":LOAD {}".format(jobname))
+
     try:
         # pdb.set_trace()
         job = imp.load_source(name, filename)
-        result = job.run()
+
+        # get timeout
+        timeout = DEFAULT_TIMEOUT
+        if 'TIMEOUT' in dir(job):
+            timeout = job.TIMEOUT
+
+        try:
+            log_it(":RUN {}; TIMEOUT {}".format(jobname, timeout))
+            with stopit.ThreadingTimeout(timeout) as tmt:
+                result = job.run()
+        except Exception as e:
+            log_it(":END {}; TIMEOUT {}; STATE timeout".format(jobname, timeout))
+            result = 'TIMEOUT'
     except Exception as e:
+        log_it(":END {}; TIMEOUT {}; STATE error".format(jobname, timeout))
         raise e
 
+    log_it(":END {}; TIMEOUT {}; STATE success".format(jobname, timeout))
     return result
+
+
+def log_it(msg):
+    """
+    @TODO: log to file
+    """
+    print(msg)
